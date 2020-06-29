f030abe7a163914c0e983e995e39cfeb225230c1
aslan <bloogrox@gmail.com>
2020-06-29 20:03:27 +0300

    setup prod database

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -3,3 +3,12 @@ from .base import *  # noqa
 
 
 SECRET_KEY = os.environ['DJ_SECRET_KEY']
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        engine='django.db.backends.postgresql_psycopg2')
+}
