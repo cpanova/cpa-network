d112a6343bd7dc9a9aa8738578a8d4e98d0027ad
aslan <bloogrox@gmail.com>
2020-08-03 22:30:08 +0300

    fix flakes

--- dictionaries/urls.py
+++ dictionaries/urls.py
@@ -4,5 +4,7 @@ from .views import countries, categories
 
 urlpatterns = [
     path('countries/', countries.CountryListView.as_view(), name='countries'),
-    path('categories/', categories.CategoryListView.as_view(), name='categories'),
+    path(
+        'categories/',
+        categories.CategoryListView.as_view(), name='categories'),
 ]
--- dictionaries/views/countries.py
+++ dictionaries/views/countries.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import permissions
 
