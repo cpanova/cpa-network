86da42b8d9a7609b44edc548fd1fa533272377a6
aslan <bloogrox@gmail.com>
2020-09-02 02:18:28 +0300

    add api-offers url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from .views.conversions import ConversionCreateView
+from .views.offer import OfferCreateView
+from .views.payout import PayoutCreateView
 
 
 urlpatterns = [
     path(
         'conversions/',
         ConversionCreateView.as_view(), name='api-conversions'),
+    path(
+        'offers/',
+        OfferCreateView.as_view(), name='api-offers'),
+    # path(
+    #     'offers/<int:pk>/payouts/',
+    #     PayoutCreateView.as_view(), name='api-payouts'),
 ]
