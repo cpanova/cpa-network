2e48bc1b72cbcaa2f346e39508987ebd6e20bc1c
aslan <bloogrox@gmail.com>
2020-09-02 02:18:02 +0300

    add api offer create endpoint and test

---
+++ api/tests/test_offer.py
@@ -0,0 +1,29 @@
+from rest_framework.test import APITestCase
+from countries_plus.models import Country
+from django.contrib.auth import get_user_model
+
+
+class OfferTestCase(APITestCase):
+    url = '/api/offers/'
+
+    def setUp(self):
+        super(OfferTestCase, self).setUp()
+        credentials = {
+            'username': 'test@test.com',
+            'password': '1234',
+        }
+        self.user = get_user_model().objects.create_user(
+            id=1, is_staff=True, is_superuser=True, **credentials)
+        self.client.login(**credentials)
+        Country.objects.create(iso='RU', iso3='RUS', iso_numeric=1, name='Russia')
+
+    def test_create(self):
+        data = {
+            "title": "test offer",
+            'countries': ['RU']
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(201, response.status_code)
+
+        offer = response.json()
+        self.assertEqual(offer['title'], "test offer")
---
+++ api/views/offer.py
@@ -0,0 +1,36 @@
+from rest_framework import status
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from ..permissions import IsSuperUser
+from offer.models import Offer
+
+
+class OfferCreationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Offer
+        fields = (
+            'id',
+            'title',
+            'description_html',
+            'tracking_link',
+            'preview_link',
+            'countries',
+            'categories',
+            # 'traffic_sources',
+            'status',
+            'advertiser',
+        )
+
+
+class OfferCreateView(APIView):
+    permission_classes = (IsAuthenticated, IsSuperUser,)
+
+    def post(self, request):
+        serializer = OfferCreationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
