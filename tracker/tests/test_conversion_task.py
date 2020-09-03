6bab33ba419f824b81c338f8dd224e2c864d2f5b
aslan <bloogrox@gmail.com>
2020-09-03 22:26:12 +0300

    test if postback with status

--- tracker/tests/test_conversion_task.py
+++ tracker/tests/test_conversion_task.py
@@ -1,36 +1,72 @@
 from django.test import TestCase
+from countries_plus.models import Country
 from ..tasks.conversion import conversion
 from ..models import Click, Conversion
 from django.contrib.auth import get_user_model
-from offer.models import Offer
+from offer.models import Offer, Payout, Currency
 
 
 class TestConversionTask(TestCase):
 
     def setUp(self):
         super(TestConversionTask, self).setUp()
-        user = (
+        self.user = (
             get_user_model().objects
             .create_user(username='aff', password='1234')
         )
-        offer = Offer.objects.create(title='blabla', description='blabla')
-        self.click = Click.objects.create(
+        self.offer = Offer.objects.create(id=1, title='blabla', description='blabla')
+
+
+    def test_conversion_create(self):
+        cl = Click.objects.create(
             ip='4.4.4.4',
             revenue=0,
             payout=0,
-            offer=offer,
-            affiliate=user
+            offer=self.offer,
+            affiliate=self.user
         )
-
-    def test_conversion_create(self):
         data = {
-            'click_id': self.click.id,
+            'click_id': cl.id,
             'goal': 1,
             'sum': 0
         }
         conversion(data)
         self.assertTrue(
             Conversion.objects
-            .filter(click_id=self.click.id)
+            .filter(click_id=cl.id)
             .count()
         )
+
+    def test_with_status(self):
+        cl = Click.objects.create(
+            ip='4.4.4.4',
+            country='US',
+            revenue=0,
+            payout=0,
+            offer=self.offer,
+            affiliate=self.user
+        )
+        p = Payout.objects.create(
+            revenue=0,
+            payout=0,
+            goal_value='1',
+            offer=cl.offer,
+            currency=Currency.objects.create(code='USD', name='USD'),
+        )
+        p.countries.add(
+            Country.objects.create(
+                iso='US', iso3='USA', iso_numeric=1, name='USA'
+            )
+        )
+        data = {
+            'click_id': cl.id,
+            'goal': 1,
+            'sum': 0,
+            'status': 'approved'
+        }
+        conversion(data)
+        cv = (
+            Conversion.objects
+            .filter(click_id=cl.id)
+            .first())
+        self.assertEquals(cv.status, 'approved')
